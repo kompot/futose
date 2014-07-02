40a5bc6ebfe889470b05433fa88a947c22a0fc55
Anton Fedchenko <kompotik@gmail.com>
2014-07-03 02:21:12 +0400

    Further gulpfile refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,55 @@ var $ = require('gulp-load-plugins')({
 });
 var webpack   = require("webpack");
 var webpackConfig = require("./webpack.config.js");
-var connect = require('connect');
+//var connect = require('connect');
 var nib = require('nib');
 var jeet = require('jeet');
 var stylusConfig = { use: [nib(), jeet()] };
 
+var srcPath    = './assets';
+var dstPath    = './build';
+var dstDevPath = './public';
+
+var paths = {
+  src: {
+    img:         srcPath + '/img/**/*',
+    cssWatch:    srcPath + '/stylus/**/*',
+    cssCompile: [srcPath + '/stylus/*.styl',
+           '!' + srcPath + '/stylus/_*.styl']
+  },
+  dev: {
+    css: dstDevPath + '/css',
+    img: dstDevPath + '/img'
+  },
+  prod: {
+    css: dstPath + '/css',
+    img: dstPath + '/img'
+  }
+};
+
 var fSrc = './assets';
 var fServerTemplates = '/template';
 var fJs = '/js';
+var fSrcImg = fSrc + '/img/*.png';
 
-gulp.task('stylus', function () {
-  gulp.src(fSrc + '/stylus/screen.styl')
+gulp.task('sprite', function () {
+  var spriteData = gulp.src(fSrc + '/sprite/*.png').pipe($.spritesmith({
+    imgName: 'sprite.png',
+    imgPath: '/img/sprite.png',
+    cssName: '_sprite.styl',
+    engine: 'pngsmith' // for maximum compatibility
+  }));
+  spriteData.img.pipe(gulp.dest(fSrc + '/img'));
+  spriteData.css.pipe(gulp.dest(fSrc + '/stylus'));
+});
+
+gulp.task('stylus', ['sprite'], function () {
+  gulp.src(paths.src.cssCompile)
     .pipe($.stylus(stylusConfig))
     .on('error', console.log)
     .pipe($.myth())
-    .pipe(gulp.dest('./public/css/'));
+    .pipe($.util.env.type === 'prod' ? $.csso() : $.util.noop())
+    .pipe(gulp.dest(paths[$.util.env.type].css));
 //    .pipe ($.livereload(server));
 });
 
@@ -39,48 +73,34 @@ gulp.task('stylus', function () {
 //});
 
 
-gulp.task('js', function () {
-  gulp.src([fSrc + fJs + '/**/*.js',
-      '!' + fSrc + fJs + '/vendor/**/*.js'])
-    .pipe($.concat('index.js'))
-    .pipe(gulp.dest('./public/js'))
-//    .pipe($.livereload(server));
-});
+//gulp.task('js', function () {
+//  gulp.src([fSrc + fJs + '/**/*.js',
+//      '!' + fSrc + fJs + '/vendor/**/*.js'])
+//    .pipe($.concat('index.js'))
+//    .pipe(gulp.dest('./public/js'))
+////    .pipe($.livereload(server));
+//});
 
-gulp.task('sprite', function () {
-  var spriteData = gulp.src(fSrc + '/sprite/*.png').pipe($.spritesmith({
-    imgName: 'sprite.png',
-    imgPath: '/img/sprite.png',
-    cssName: 'sprite.styl',
-    engine: 'pngsmith' // for maximum compatibility
-  }));
-  spriteData.img.pipe(gulp.dest(fSrc + '/img'));
-  spriteData.css.pipe(gulp.dest(fSrc + '/stylus'));
-});
-
-gulp.task('images', function () {
-  gulp.src(fSrc + '/img/**/*')
+gulp.task('images', ['sprite'], function () {
+  gulp.src(paths.src.img)
     .pipe($.imagemin())
-    .pipe(gulp.dest('./public/img'))
+    .pipe(gulp.dest(paths[$.util.env.type].img));
 });
 
 var spawn = require('child_process').spawn;
 var node;
 
 gulp.task('http-server', function () {
-//  connect()
-//    .use(require('connect-livereload')())
-//    .use(connect.static('./public'))
-//    .listen('9000');
   node = spawn('nodemon', ['server.js'], { stdio: 'inherit' });
   console.log('Server listening on http://127.0.0.1:9001');
 });
 
-var myDevConfig = Object.create(webpackConfig);
-myDevConfig.devtool = "sourcemap";
-//myDevConfig.devtool = "eval";
-myDevConfig.debug = true;
-var devCompiler = webpack(myDevConfig);
+var devConfig = Object.create(webpackConfig);
+devConfig.devtool = "sourcemap";
+//devConfig.devtool = "eval";
+devConfig.debug = true;
+var devCompiler = webpack(devConfig);
+
 gulp.task('webpack:build-dev', function(callback) {
   devCompiler.run(function(err, stats) {
     if(err) throw new gutil.PluginError('webpack:build-dev', err);
@@ -91,60 +111,46 @@ gulp.task('webpack:build-dev', function(callback) {
   });
 });
 
-gulp.task('default', function () {
-  gulp.run('sprite');
-  gulp.run('stylus');
-//  gulp.run('jade');
-  gulp.run('images');
-//  gulp.run('js');
+
+gulp.task('default', ['images', 'stylus'], function () {
+  $.util.env.type = 'dev';
+
+  gulp.watch(paths.src.img, ['images']);
+  gulp.watch(paths.src.cssWatch, ['stylus']);
+  gulp.watch(fSrc + '/sprite/*.png', ['sprite']);
+
+  gulp.watch(fSrc + fJs + '/**/*', ['webpack:build-dev']);
+
   gulp.run('webpack:build-dev');
   gulp.run('fb-flo');
-
-  gulp.watch(fSrc + '/stylus/**/*.styl', ['stylus']);
-  gulp.watch(fSrc + fJs + '/**/*', ['webpack:build-dev']);
-  gulp.watch(fSrc + '/sprite/*', ['sprite']);
-
-  server.listen(35729, function (err) {
-    if (err) return console.log(err);
-
-//    gulp.watch('assets/stylus/**/*.styl', function() {
-//        gulp.run('stylus');
-//    });
-    gulp.watch(fSrc + fServerTemplates + '/**/*.jade', function () {
-      console.log('wach!!!!!!!!!!!!!!');
-//      gulp.run('jade');
-    });
-//    gulp.watch('assets/img/**/*', function() {
-//        gulp.run('images');
-//    });
-//    gulp.watch('assets/js/**/*', function() {
-//        gulp.run('js');
-//    });
-  });
   gulp.run('http-server');
 });
 
-gulp.task('build', function () {
-  gulp.src(fSrc + '/stylus/screen.styl')
-    .pipe($.stylus(stylusConfig))
-    .pipe($.myth())
-    .pipe($.csso())
-    .pipe(gulp.dest('./build/css/'));
+gulp.task('build', ['stylus'], function () {
+  $.util.env.type = 'prod';
+
+
+
+//  gulp.src(fSrc + '/stylus/screen.styl')
+//    .pipe($.stylus(stylusConfig))
+//    .pipe($.myth())
+//    .pipe($.csso())
+//    .pipe(gulp.dest('./build/css/'));
 
 //  gulp.src([fSrc + fServerTemplates + '/*.jade',
 //      '!' + fSrc + fServerTemplates + '/_*.jade'])
 //    .pipe($.jade())
 //    .pipe(gulp.dest('./build/'));
 
-  gulp.src([fSrc + fJs + '/**/*.js',
-      '!' + fSrc + fJs + '/vendor/**/*.js'])
-    .pipe($.concat('index.js'))
-    .pipe($.uglify())
-    .pipe(gulp.dest('./build/js'));
-
-  gulp.src(fSrc + '/img/**/*')
-    .pipe($.imagemin())
-    .pipe(gulp.dest('./build/img'))
+//  gulp.src([fSrc + fJs + '/**/*.js',
+//      '!' + fSrc + fJs + '/vendor/**/*.js'])
+//    .pipe($.concat('index.js'))
+//    .pipe($.uglify())
+//    .pipe(gulp.dest('./build/js'));
+//
+//  gulp.src(paths.img)
+//    .pipe($.imagemin())
+//    .pipe(gulp.dest('./build/img'))
 });
 
 gulp.task('fb-flo', function () {
